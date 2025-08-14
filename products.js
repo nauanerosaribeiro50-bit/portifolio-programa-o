e919071dec604954af3222ae3f5a82472c95903d
nauanerosaribeiro50-bit <nauanerosaribeiro50@gmail.com>
2025-08-14 16:53:15 -0300

    site completo de e-commerce

---
+++ products.js
@@ -0,0 +1,15 @@
+const products = [
+  { id: 1, name: "Smartphone X Pro", price: 2899.99, image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&w=400&q=80" },
+  { id: 2, name: "Fone de Ouvido Wireless", price: 399.90, image: "https://dcdn-us.mitiendanube.com/stores/002/475/768/products/br-11134207-7r98p-lmlcbfzg5dt7dd-c770158a055b015e1517067961308512-1024-1024.jpg" },
+  { id: 3, name: "Smart TV 50 Polegadas", price: 2599.00, image: "https://imgs.casasbahia.com.br/55066184/1g.jpg" },
+  { id: 4, name: "Notebook Gamer", price: 4999.90, image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=400&q=80" },
+  { id: 5, name: "Smartwatch Série 5", price: 1299.00, image: "https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch_series_5-gold-aluminum-case-pomegranate-band-and-space-gray-aluminum-case-pine-green-band-091019_big.jpg.large.jpg" },
+
+  { id: 6, name: "Tablet Ultra", price: 1500.00, image: "https://m.media-amazon.com/images/I/41FD+UCEn3L._UF894,1000_QL80_.jpg" },
+  { id: 7, name: "Câmera Digital Pro", price: 2100.00, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY2SwKj5ZIJ6dIuIB4vmJNDGS8hrNQGR83dw&s" },
+  { id: 8, name: "Caixa de Som Bluetooth", price: 350.00, image: "https://lojasbecker.vtexassets.com/arquivos/ids/3712332-800-auto?v=638739515469630000&width=800&height=auto&aspect=true" },
+  { id: 9, name: "Console de Videogame", price: 1800.00, image: "https://s2-techtudo.glbimg.com/CxNLOWCwew0HitVXxR30JwhQNcE=/0x0:1449x948/984x0/smart/filters:strip_icc()/s.glbimg.com/po/tt2/f/original/2016/04/12/new-cdi.png" },
+  { id: 10, name: "Monitor 27 Polegadas", price: 1200.00, image: "https://m.media-amazon.com/images/I/610AB4mxF6L.jpg" },
+  { id: 11, name: "Teclado Mecânico", price: 450.00, image: "https://cdn.sistemawbuy.com.br/arquivos/a4456ac015133534fb513a1cb95ceb43/produtos/67008d67b40ea/tec-tom-3-6700941d8ca52.jpg" },
+  { id: 12, name: "Mouse Gamer RGB", price: 300.00, image: "https://b2bleonorashop.vtexassets.com/arquivos/ids/161285-800-auto?v=638067308343670000&width=800&height=auto&aspect=true" }
+];
